dc1575c88c756822efa623e8c90fbeac40562cef
mghoneem10 <mghoneem10@gmail.com>
2021-07-12 18:49:34 +0000

    1st release

---
+++ injaz/injaz/report/unavilable/unavilable.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2019, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Unavilable"] = {
+	"filters": [
+		{
+			fieldname:"from_date",
+			reqd: 1,
+			label: __("From Date"),
+			fieldtype: "Date",
+			default: frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+		},
+		{
+			fieldname:"to_date",
+			reqd: 1,
+			default: frappe.datetime.get_today(),
+			label: __("To Date"),
+			fieldtype: "Date",
+		},
+	
+	],
+
+	
+};
